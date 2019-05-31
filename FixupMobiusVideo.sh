99cb4ce6380e46b1ef2a39a537a452059ae6173b
T.J. Mercier <thomas.mercier.jr@gmail.com>
2019-05-30 22:18:18 -0700

    Expand video stream duration to match audio stream duration

--- FixupMobiusVideo.sh
+++ FixupMobiusVideo.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
 
 # Mobius Action Cam 2 videos have audio streams that are longer than the video streams.
-# This script demuxes the video, truncates the audio stream to the length of the video stream, and then remuxes the streams to allow for glitch-free video concatenation without encoding.
-
-function finish
-{
-  rm -rf "$tmpdir"
-}
-trap finish EXIT
+# The audio streams are the correct length. The presentation time stamps on the video streams
+# are incorrect. This script re-muxes the video after discarding the original presentation
+# time stamps, and has the muxer generate new ones based on a scaled video frame rate.
 
 if [ "$#" -lt 1 ]; then
   echo "Use: $0 path/to/video.mp4 [path/to/video2.mp4] ..."
@@ -15,27 +11,10 @@ if [ "$#" -lt 1 ]; then
 fi
 
 for arg; do
-  tmpdir=$(mktemp -d)
+  video_duration_in_frames=$(ffprobe -hide_banner -i "$arg" -show_streams -select_streams v|grep nb_frames=|cut -f2 -d=)
+  audio_duration_in_sec=$(ffprobe -hide_banner -i "$arg" -show_streams -select_streams a|grep duration=|cut -f2 -d=)
 
-  # Demux the video
-  ffmpeg -hide_banner -i "$arg" -c:v copy -map 0:0 "$tmpdir/video.m4v" -c:a copy -map 0:1 "$tmpdir/audio.m4a"
+  new_frame_rate=$(bc -l <<< "$video_duration_in_frames/$audio_duration_in_sec")
 
-  # Get video duration
-  video_duration_in_sec=$(ffprobe -hide_banner -i "$tmpdir/video.m4v" -show_streams|grep duration=|cut -f2 -d=)
-
-  # Truncate audio to video duration
-  # This will cut the audio to the closest boundary to the time specified, but may be up to a frame length past it.
-  ffmpeg -hide_banner -i "$tmpdir/audio.m4a" -t $video_duration_in_sec -c:a copy "$tmpdir/audio_trunc.m4a"
-
-  # If the audio duration is still longer than the video duration, remove the last frame from the audio
-  audio_trunc_duration_in_sec=$(ffprobe -hide_banner -i "$tmpdir/audio_trunc.m4a" -show_streams|grep duration=|cut -f2 -d=)
-  if [[ "$audio_trunc_duration_in_sec" > "$video_duration_in_sec" ]]; then
-    echo "Still need to remove a frame"
-    audio_trunc_frames=$(ffprobe -hide_banner -i "$tmpdir/audio_trunc.m4a" -show_streams|grep nb_frames|cut -f2 -d=)
-    target_frame_count=$(echo $(( audio_trunc_frames - 1 )))
-    target_audio_duration=$(bc -l <<< $audio_trunc_duration_in_sec*$target_frame_count/$audio_trunc_frames)
-    ffmpeg -hide_banner -y -i "$tmpdir/audio.m4a" -t $target_audio_duration -c:a copy "$tmpdir/audio_trunc.m4a"
-  fi
-
-  ffmpeg -i "$tmpdir/video.m4v" -i "$tmpdir/audio_trunc.m4a" -c:v copy -c:a copy "$(echo "$arg"|sed 's,\(.*\)\.\([^\.]*\),\1_FIXED.\2,g')"
+  ffmpeg -i "$arg" -r $new_frame_rate -codec copy -vsync drop "$(echo "$arg"|sed 's,\(.*\)\.\([^\.]*\),\1_FIXED.\2,g')"
 done
